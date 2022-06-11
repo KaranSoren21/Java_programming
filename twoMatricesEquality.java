a50739632e7fb6fb49e8f9586152d960eee760ef
Karan Soren <70742988+KaranSoren21@users.noreply.github.com>
2022-06-11 18:32:28 +0530

    Update twoMatricesEquality.java

--- twoMatricesEquality.java
+++ twoMatricesEquality.java
@@ -5,11 +5,11 @@ public class twoMatricesEquality {
 		// TODO Auto-generated method stub
 		/*
 		 * A = |1 2 3|
-		 * 	   |2 3 4|
-		 * 	   |1 1 1|
+		 *     |2 3 4|
+		 *     |1 1 1|
 		 * 
 		 * B = |4 5 6|
-		 * 	   |7 8 9|
+		 *     |7 8 9|
 		 *     |4 5 7|
 		 * */		
 		int [][] A = {{1,2,3},{2,3,4},{1,1,1}};
